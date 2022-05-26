c79ecd344f166aac1a41e4fd00eab5dac5cda4b2
sshinohs <sshinohs@gmail.com>
2022-05-26 19:17:05 +0900

    주식 검색 기능 추가

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy as db
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+app.config['JSON_AS_ASCII'] = False
 
 engine = db.create_engine(app.config['DB_URL'], encoding='utf-8')
 
@@ -12,36 +13,6 @@ CORS(app, resources={r'*': {'origins': ['http://localhost:3000', 'http://127.0.0
 connection = engine.connect()
 metadata = db.MetaData()
 table = db.Table('items', metadata, autoload=True, autoload_with=engine)
-
-query = db.select([table])
-
-print('query')
-print(query)
-
-result_proxy = connection.execute(query)
-result_set = result_proxy.fetchall()
-
-print('테이블 내용 (result_set[:10]')
-print(result_set[:10])
-
-query = db.select([table]).where(table.columns.name == '삼성전자')
-result_proxy = connection.execute(query)
-result_set = result_proxy.fetchall()
-
-print('삼성전자 찾기')
-print(result_set[:10])
-
-query = db.select([table]).where(table.columns.name == '삼성전자')
-
-print('삼성전자 찾기의 query')
-print(query)
-result_proxy = connection.execute(query)
-result_set = result_proxy.fetchall()
-
-print('삼성전자 찾기')
-print(result_set[:10])
-
-print(table.columns.keys())
 
 
 @app.route('/hello_world')
@@ -58,7 +29,17 @@ def home_page():
 def item_search():
     order = request.args.get('keyword')
     if order:
-        return order + ' zzang!'
+        query = f"SELECT * FROM items WHERE name LIKE '%{order}%';"
+        result_proxy = connection.execute(query)
+        result_set = result_proxy.fetchall()
+
+        result_list = []
+        for result in result_set:
+            result_list.append({
+                'name': result.name,
+                'code': result.code
+            })
+        return jsonify(result_list)
 
 
 @app.route('/echo_call/<param>')
